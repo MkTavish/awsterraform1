83839b08807cabdeceaa9bbc22370c1037414244
EC2 Default User <ec2-user@ip-172-31-91-210.ec2.internal>
2019-11-18 06:10:51 +0000

    updated files to uses resources from the networking module

--- main.tf
+++ main.tf
@@ -7,4 +7,12 @@ provider "aws" {
 module "storage" {
   source       = "./storage"
   project_name = "${var.project_name}"
-}:
+}
+
+# Deploy Networking Resources
+module "networking" {
+  source       = "./networking"
+  vpc_cidr     = "${var.vpc_cidr}"
+  public_cidrs = "${var.public_cidrs}"
+  accessip     = "${var.accessip}"
+}
